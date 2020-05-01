b2b23e7d89830303aae5eecdb355cacd8f7c50aa
Michal Schulz <michal.schulz@gmx.de>
2020-05-01 23:59:38 +0200

    Created first part of translations - they are automatically generated from Qt source files and Qt UI files

---
+++ i18n/DSS_cs.ts
@@ -0,0 +1,196 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cs_CZ">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../qt/About.ui" line="14"/>
+        <source>About DeepSkyStacker</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt/About.ui" line="36"/>
+        <source>Language</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt/About.ui" line="44"/>
+        <source>Use the following language at next startup</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt/About.ui" line="105"/>
+        <source>Check if newer version is available at startup</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt/About.cpp" line="12"/>
+        <source>DeepSkyStacker version %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt/About.cpp" line="21"/>
+        <source>RAW file decoding by LibRaw (version %1)
+Copyright © 1997-2019 LibRaw LLC</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt/About.cpp" line="28"/>
+        <source>TIFF file encoding/decoding by LibTIFF (version %1)
+Copyright © 1988-1996 Sam Leffler
+Copyright © 1991-1996 Silicon Graphics, Inc.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt/About.cpp" line="35"/>
+        <source>FITS decoding by CFitsIO (version %1)
+Copyright NASA</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AlignmentParameters</name>
+    <message>
+        <location filename="../qt/AlignmentParameters.ui" line="14"/>
+        <source>Alignment</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt/AlignmentParameters.ui" line="26"/>
+        <source>Transformation used during alignment</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt/AlignmentParameters.ui" line="37"/>
+        <source>Automatic</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt/AlignmentParameters.ui" line="44"/>
+        <source>Bilinear</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt/AlignmentParameters.ui" line="51"/>
+        <source>Bisquared</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt/AlignmentParameters.ui" line="58"/>
+        <source>Bicubic</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt/AlignmentParameters.ui" line="65"/>
+        <source>No alignment</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt/AlignmentParameters.cpp" line="23"/>
+        <source>Automatic
+
+The alignment method is automatically selected depending on the number of available stars.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt/AlignmentParameters.cpp" line="26"/>
+        <source>Bilinear Alignment
+
+The Bilinear Alignment is used in all cases.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt/AlignmentParameters.cpp" line="29"/>
+        <source>Bisquared Alignment
+
+The Bisquared Alignment is used when at least 25 stars are available, else the Bilinear Alignment method is used.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt/AlignmentParameters.cpp" line="32"/>
+        <source>Bicubic Alignment
+
+The Bicubic method is used when at least 40 stars are available, then the Bisquared method is used if 25 to 39 stars are available, then the Bilinear method is used when less than 25 stars are available.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AskRegistering</name>
+    <message>
+        <location filename="../qt/AskRegistering.ui" line="14"/>
+        <source>Unregistered light frame</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt/AskRegistering.ui" line="37"/>
+        <source>This light frame is not registered
+(the stars have not been detected).
+
+Do you want...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt/AskRegistering.ui" line="66"/>
+        <source>to register this light frame</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt/AskRegistering.ui" line="73"/>
+        <source>to register all the light frames</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt/AskRegistering.ui" line="80"/>
+        <source>to continue without registering the light frame</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>BackgroundOptions</name>
+    <message>
+        <location filename="../qt/BackgroundOptions.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt/BackgroundOptions.ui" line="20"/>
+        <source>Calibration Method</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt/BackgroundOptions.ui" line="28"/>
+        <source>Linear</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt/BackgroundOptions.ui" line="35"/>
+        <source>Rational</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt/BackgroundOptions.ui" line="60"/>
+        <source>RGB Background Calibration Method</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt/BackgroundOptions.ui" line="68"/>
+        <source>None</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt/BackgroundOptions.ui" line="75"/>
+        <source>Minimum</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt/BackgroundOptions.ui" line="82"/>
+        <source>Middle</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt/BackgroundOptions.ui" line="89"/>
+        <source>Maximum</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ i18n/DSS_zh.ts
@@ -0,0 +1,206 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../qt/About.ui" line="14"/>
+        <source>About DeepSkyStacker</source>
+        <translation>關於DeepSkyStacker</translation>
+    </message>
+    <message>
+        <location filename="../qt/About.ui" line="36"/>
+        <source>Language</source>
+        <translation>語系</translation>
+    </message>
+    <message>
+        <location filename="../qt/About.ui" line="44"/>
+        <source>Use the following language at next startup</source>
+        <translation>下次啟動時使用此語系</translation>
+    </message>
+    <message>
+        <location filename="../qt/About.ui" line="105"/>
+        <source>Check if newer version is available at startup</source>
+        <translation>啟動時檢查是否有新版本</translation>
+    </message>
+    <message>
+        <location filename="../qt/About.cpp" line="12"/>
+        <source>DeepSkyStacker version %1</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qt/About.cpp" line="21"/>
+        <source>RAW file decoding by LibRaw (version %1)
+Copyright © 1997-2019 LibRaw LLC</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qt/About.cpp" line="28"/>
+        <source>TIFF file encoding/decoding by LibTIFF (version %1)
+Copyright © 1988-1996 Sam Leffler
+Copyright © 1991-1996 Silicon Graphics, Inc.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qt/About.cpp" line="35"/>
+        <source>FITS decoding by CFitsIO (version %1)
+Copyright NASA</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>AlignmentParameters</name>
+    <message>
+        <location filename="../qt/AlignmentParameters.ui" line="14"/>
+        <source>Alignment</source>
+        <translation type="unfinished">對齊星點</translation>
+    </message>
+    <message>
+        <location filename="../qt/AlignmentParameters.ui" line="26"/>
+        <source>Transformation used during alignment</source>
+        <translation type="unfinished">對齊各影像中的星點時所用的轉換法</translation>
+    </message>
+    <message>
+        <location filename="../qt/AlignmentParameters.ui" line="37"/>
+        <source>Automatic</source>
+        <translation type="unfinished">系統自動決定</translation>
+    </message>
+    <message>
+        <location filename="../qt/AlignmentParameters.ui" line="44"/>
+        <source>Bilinear</source>
+        <translation type="unfinished">雙線性</translation>
+    </message>
+    <message>
+        <location filename="../qt/AlignmentParameters.ui" line="51"/>
+        <source>Bisquared</source>
+        <translation type="unfinished">雙二次方</translation>
+    </message>
+    <message>
+        <location filename="../qt/AlignmentParameters.ui" line="58"/>
+        <source>Bicubic</source>
+        <translation type="unfinished">雙三次方</translation>
+    </message>
+    <message>
+        <location filename="../qt/AlignmentParameters.ui" line="65"/>
+        <source>No alignment</source>
+        <translation type="unfinished">不執行對齊各幅星點</translation>
+    </message>
+    <message>
+        <location filename="../qt/AlignmentParameters.cpp" line="23"/>
+        <source>Automatic
+
+The alignment method is automatically selected depending on the number of available stars.</source>
+        <translation type="unfinished">自動
+
+視星星數目自動決定星點對齊方式。</translation>
+    </message>
+    <message>
+        <location filename="../qt/AlignmentParameters.cpp" line="26"/>
+        <source>Bilinear Alignment
+
+The Bilinear Alignment is used in all cases.</source>
+        <translation type="unfinished">雙線性
+
+一律使用雙線性法對齊星點。</translation>
+    </message>
+    <message>
+        <location filename="../qt/AlignmentParameters.cpp" line="29"/>
+        <source>Bisquared Alignment
+
+The Bisquared Alignment is used when at least 25 stars are available, else the Bilinear Alignment method is used.</source>
+        <translation type="unfinished">雙平方
+
+在有超過25個星點時使用雙平方法，否則使用雙線性。</translation>
+    </message>
+    <message>
+        <location filename="../qt/AlignmentParameters.cpp" line="32"/>
+        <source>Bicubic Alignment
+
+The Bicubic method is used when at least 40 stars are available, then the Bisquared method is used if 25 to 39 stars are available, then the Bilinear method is used when less than 25 stars are available.</source>
+        <translation type="unfinished">雙立方
+
+超過40個星點時使用雙立方法，25到39個星點時使用雙平方，25以下使用雙線性。</translation>
+    </message>
+</context>
+<context>
+    <name>AskRegistering</name>
+    <message>
+        <location filename="../qt/AskRegistering.ui" line="14"/>
+        <source>Unregistered light frame</source>
+        <translation type="unfinished">尚未偵測星點的天體影像</translation>
+    </message>
+    <message>
+        <location filename="../qt/AskRegistering.ui" line="37"/>
+        <source>This light frame is not registered
+(the stars have not been detected).
+
+Do you want...</source>
+        <translation type="unfinished">本天體影像尚未通過星點偵測
+
+您要....</translation>
+    </message>
+    <message>
+        <location filename="../qt/AskRegistering.ui" line="66"/>
+        <source>to register this light frame</source>
+        <translation type="unfinished">偵測本幅影像中的星點</translation>
+    </message>
+    <message>
+        <location filename="../qt/AskRegistering.ui" line="73"/>
+        <source>to register all the light frames</source>
+        <translation type="unfinished">偵測所有影像中的星點</translation>
+    </message>
+    <message>
+        <location filename="../qt/AskRegistering.ui" line="80"/>
+        <source>to continue without registering the light frame</source>
+        <translation type="unfinished">繼續處理但不偵測本幅影像中的星點</translation>
+    </message>
+</context>
+<context>
+    <name>BackgroundOptions</name>
+    <message>
+        <location filename="../qt/BackgroundOptions.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qt/BackgroundOptions.ui" line="20"/>
+        <source>Calibration Method</source>
+        <translation type="unfinished">校正方法</translation>
+    </message>
+    <message>
+        <location filename="../qt/BackgroundOptions.ui" line="28"/>
+        <source>Linear</source>
+        <translation type="unfinished">線性</translation>
+    </message>
+    <message>
+        <location filename="../qt/BackgroundOptions.ui" line="35"/>
+        <source>Rational</source>
+        <translation type="unfinished">有理數</translation>
+    </message>
+    <message>
+        <location filename="../qt/BackgroundOptions.ui" line="60"/>
+        <source>RGB Background Calibration Method</source>
+        <translation type="unfinished">RGB背景校正方式</translation>
+    </message>
+    <message>
+        <location filename="../qt/BackgroundOptions.ui" line="68"/>
+        <source>None</source>
+        <translation type="unfinished">無</translation>
+    </message>
+    <message>
+        <location filename="../qt/BackgroundOptions.ui" line="75"/>
+        <source>Minimum</source>
+        <translation type="unfinished">極小值</translation>
+    </message>
+    <message>
+        <location filename="../qt/BackgroundOptions.ui" line="82"/>
+        <source>Middle</source>
+        <translation type="unfinished">中間值</translation>
+    </message>
+    <message>
+        <location filename="../qt/BackgroundOptions.ui" line="89"/>
+        <source>Maximum</source>
+        <translation type="unfinished">最大值</translation>
+    </message>
+</context>
+</TS>
