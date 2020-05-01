1445e9dd7e3fea24c4fc0d53d893928e79f784bb
Michal Schulz <michal.schulz@gmx.de>
2020-05-02 00:02:19 +0200

    Set translations to the defalut language, show About window

--- qt/main.cpp
+++ qt/main.cpp
@@ -1,14 +1,24 @@
 #include <QApplication>
+#include <QTranslator>
 #include "AskRegistering.h"
 #include "AlignmentParameters.h"
+#include "BackgroundOptions.h"
+#include "About.h"
 
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
-    AlignmentParameters ask;
+    QTranslator translator;
+    QString language = QLocale().name().left(2);
+
+    if (translator.load(":/i18n/DSS_" + language + ".qm"))
+    {
+        app.installTranslator(&translator);
+    }
+
+    About ask;
     
     ask.show();
     
     return app.exec();
 }
-
