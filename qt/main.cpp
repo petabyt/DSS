ad92343be13d85153cc1933d5062059090ff8849
Michal Schulz <michal.schulz@gmx.de>
2020-05-02 09:03:05 +0200

    Honour language selected by the user, otherwise take the system default

--- qt/main.cpp
+++ qt/main.cpp
@@ -1,9 +1,11 @@
 #include <QApplication>
 #include <QTranslator>
+#include <QSettings>
 #include "AskRegistering.h"
 #include "AlignmentParameters.h"
 #include "BackgroundOptions.h"
 #include "About.h"
+#include "DeepStackerDlg.h"
 
 int main(int argc, char **argv)
 {
@@ -14,16 +16,23 @@ int main(int argc, char **argv)
     Q_UNUSED(QT_TRANSLATE_NOOP("QPlatformTheme", "&No"));
     Q_UNUSED(QT_TRANSLATE_NOOP("QPlatformTheme", "OK"));
 
+    QCoreApplication::setOrganizationName("DeepSkyStacker");
+    QCoreApplication::setOrganizationDomain("deepskystacker.free.fr");
+    QCoreApplication::setApplicationName("DeepSkyStacker");
+
     QApplication app(argc, argv);
     QTranslator translator;
-    QString language = QLocale().name().left(2);
+    QString language = QSettings().value("Language", "").toString();
+    
+    if (language == "")
+        language = QLocale().name().left(2);
 
     if (translator.load(":/i18n/DSS_" + language + ".qm"))
     {
         app.installTranslator(&translator);
     }
 
-    BackgroundOptions ask;
+    DeepStackerDlg ask;
     
     ask.show();
     
