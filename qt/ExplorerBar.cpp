b2f4957415dc1a5ad11cbab668cfc860598209f9
Michal Schulz <michal.schulz@gmx.de>
2020-05-02 10:23:57 +0200

    Partially implemented ExplorerBar

---
+++ qt/ExplorerBar.cpp
@@ -0,0 +1,29 @@
+#include "ClickLabel.h"
+#include "ExplorerBar.h"
+#include "ui_ExplorerBar.h"
+#include "About.h"
+#include <iostream>
+
+ExplorerBar::ExplorerBar(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::ExplorerBar)
+{
+    ui->setupUi(this);
+}
+
+ExplorerBar::~ExplorerBar()
+{
+    delete ui;
+}
+
+void ExplorerBar::linkActivated()
+{
+    std::cout << "linkActivated: " << std::endl;
+}
+
+void ExplorerBar::onAbout()
+{
+    About dlg(this);
+
+    dlg.exec();
+}
---
+++ qt/ExplorerBar.h
@@ -0,0 +1,27 @@
+#ifndef EXPLORERBAR_H
+#define EXPLORERBAR_H
+
+#include <QWidget>
+#include "ClickLabel.h"
+
+namespace Ui {
+class ExplorerBar;
+}
+
+class ExplorerBar : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit ExplorerBar(QWidget *parent = nullptr);
+    ~ExplorerBar();
+
+private slots:
+    void onAbout();
+    void linkActivated();
+
+private:
+    Ui::ExplorerBar *ui;
+};
+
+#endif // EXPLORERBAR_H
