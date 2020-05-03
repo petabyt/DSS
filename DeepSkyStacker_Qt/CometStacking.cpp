4709cfcfbd876949e57659f2a764746f2eb20941
Michal Schulz <michal.schulz@gmx.de>
2020-05-03 23:31:20 +0200

    comet stacking mechanic is working

--- DeepSkyStacker_Qt/CometStacking.cpp
+++ DeepSkyStacker_Qt/CometStacking.cpp
@@ -12,3 +12,43 @@ CometStacking::~CometStacking()
 {
     delete ui;
 }
+
+void CometStacking::onBnClickedStandardStacking(bool on)
+{
+    if (on) {
+        setCometStackingMode(CSM_STANDARD);
+    }
+}
+
+void CometStacking::onBnClickedCometStacking(bool on)
+{
+    if (on) {
+        setCometStackingMode(CSM_COMETONLY);
+    }
+}
+
+void CometStacking::onBnClickedAdvancedStacking(bool on)
+{
+    if (on) {
+        setCometStackingMode(CSM_COMETSTAR);
+    }
+}
+
+void CometStacking::updateImage()
+{
+    if (m_CometStackingMode == CSM_STANDARD)
+    {
+        QPixmap pm(":/comet/normal.bmp");
+        ui->laComet->setPixmap(pm);
+    }
+    else if (m_CometStackingMode == CSM_COMETONLY)
+    {
+        QPixmap pm(":/comet/trails.bmp");
+        ui->laComet->setPixmap(pm);
+    }
+    else 
+    {
+        QPixmap pm(":/comet/freeze.bmp");
+        ui->laComet->setPixmap(pm);
+    }
+}
--- DeepSkyStacker_Qt/CometStacking.h
+++ DeepSkyStacker_Qt/CometStacking.h
@@ -7,16 +7,49 @@ namespace Ui {
 class CometStacking;
 }
 
+typedef enum tagCOMETSTACKINGMODE
+{
+        CSM_STANDARD    = 0,
+        CSM_COMETONLY   = 1,
+        CSM_COMETSTAR   = 2
+}COMETSTACKINGMODE;
+
 class CometStacking : public QWidget
 {
     Q_OBJECT
+    Q_PROPERTY(COMETSTACKINGMODE cometStackingMode READ cometStackingMode WRITE setCometStackingMode NOTIFY cometStackingModeChanged);
 
 public:
     explicit CometStacking(QWidget *parent = nullptr);
     ~CometStacking();
 
+    void setCometStackingMode(COMETSTACKINGMODE mode)
+    {
+        if (mode != m_CometStackingMode)
+        {
+            m_CometStackingMode = mode;
+            updateImage();
+            emit cometStackingModeChanged();
+        }
+    }
+
+    COMETSTACKINGMODE cometStackingMode()
+    {
+        return m_CometStackingMode;
+    }
+
+signals:
+    void cometStackingModeChanged();
+
+private slots:
+    void onBnClickedStandardStacking(bool);
+    void onBnClickedCometStacking(bool);
+    void onBnClickedAdvancedStacking(bool);
+
 private:
     Ui::CometStacking *ui;
+    COMETSTACKINGMODE m_CometStackingMode;
+    void updateImage();
 };
 
 #endif // COMETSTACKING_H
