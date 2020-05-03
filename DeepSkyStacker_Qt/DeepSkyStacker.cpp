64eee017b3d83a747292b1640283538515db186c
Michal Schulz <michal.schulz@gmx.de>
2020-05-03 11:56:56 +0200

    Main file - checks whether it is the only instalce of the app, catch exceptions and displays warning message in case of one caught.

--- DeepSkyStacker_Qt/DeepSkyStacker.cpp
+++ DeepSkyStacker_Qt/DeepSkyStacker.cpp
@@ -1,11 +1,16 @@
 #include <QApplication>
 #include <QTranslator>
 #include <QSettings>
+#include <QSharedMemory>
+#include <QDebug>
+#include <QMessageBox>
+#include <QString>
 #include "AskRegistering.h"
 #include "AlignmentParameters.h"
 #include "BackgroundOptions.h"
 #include "About.h"
 #include "DeepStackerDlg.h"
+#include <exception>
 
 int main(int argc, char **argv)
 {
@@ -21,20 +26,41 @@ int main(int argc, char **argv)
     QCoreApplication::setApplicationName("DeepSkyStacker");
 
     QApplication app(argc, argv);
+    QSharedMemory hMutex("DeepSkyStacker.Mutex.UniqueID.12354687");
+    bool bFirstInstance = true;
+    int nRetCode = 0;
+
+    /* Attempt to create shared memory segment with the key */
+    if (!hMutex.create(1))
+        bFirstInstance = false;
+
+    /* Install translation */
     QTranslator translator;
     QString language = QSettings().value("Language", "").toString();
     
-    if (language == "")
+    /* Language was not defined in preferences. Select the system default */
+    if (language == "") {
         language = QLocale().name().left(2);
+    }
 
-    if (translator.load(":/i18n/DSS_" + language + ".qm"))
-    {
+    /* Install language if the translation is existing */
+    if (translator.load(":/i18n/DSS_" + language + ".qm")) {
         app.installTranslator(&translator);
     }
 
-    DeepStackerDlg ask;
-    
-    ask.show();
-    
-    return app.exec();
+    try {
+        DeepStackerDlg dlg;
+        dlg.show();
+        nRetCode = app.exec();
+    }
+    catch (std::exception &e)
+    {
+        QMessageBox::critical(nullptr, "DeepSkyStacker", QString(QCoreApplication::translate("main", "Exception caught:\n\n%1")).arg(e.what()));
+    }
+    catch (...)
+    {
+        QMessageBox::critical(nullptr, "DeepSkyStacker", QCoreApplication::translate("main", "Unknown exception caught"));
+    }
+
+    return nRetCode;
 }
