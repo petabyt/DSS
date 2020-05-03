36e0de03244ba3984b9fb799099e3808870b1650
Michal Schulz <michal.schulz@gmx.de>
2020-05-03 09:33:59 +0200

    use version number from the DSSVersion.h

--- qt/DeepStackerDlg.cpp
+++ qt/DeepStackerDlg.cpp
@@ -1,12 +1,13 @@
 #include "DeepStackerDlg.h"
 #include "ui_DeepStackerDlg.h"
+#include "DSSVersion.h"
 
 DeepStackerDlg::DeepStackerDlg(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::DeepStackerDlg)
 {
     ui->setupUi(this);
-    setWindowTitle(QString(tr("DeepSkyStacker %1")).arg("1.0.0.1" /* VERSION_DEEPSKYSTACKER */));
+    setWindowTitle(QString(tr("DeepSkyStacker %1")).arg(VERSION_DEEPSKYSTACKER));
     m_ExplorerBar = new ExplorerBar(this);
     ui->layoutLeft->addWidget(m_ExplorerBar);
 }
