2aa3f8a8eddfaefc55487d692b2e7001c04f9b3f
Michal Schulz <michal.schulz@gmx.de>
2020-05-04 23:26:25 +0200

    added flags to the resource, added language credits. Added saving/restoring of the internet check setting.

--- DeepSkyStacker_Qt/About.cpp
+++ DeepSkyStacker_Qt/About.cpp
@@ -69,13 +69,70 @@ About::About(QWidget *parent) :
         ui->comboBox->addItem(langName, lang);
     }
     setLanguage(QSettings().value("Language", "").toString());
-
+    ui->cbCheckUpdate->setChecked(QSettings().value("InternetCheck", false).toBool());
     ui->html->setText(strHTML);
+
+    strHTML  = "<img valign=center src=\"qrc:///flags/spanish.png\">&nbsp;&nbsp;";
+    strHTML += tr("Spanish version: Ricardo Contreras and the team from") + " ";
+    strHTML += QString("<a href=\"http://espacioprofundo.com.ar/\">%1</a><br/>").arg(tr("Espacio Profundo"));
+
+    strHTML += "<img valign=center src=\"qrc:///flags/czech.png\">&nbsp;&nbsp;";
+    strHTML += tr("Czech version:") + " ";
+    strHTML += QString("<a href=\"mailto:lukavsky.premysl@tiscali.cz\">%1</a><br/>").arg(tr("Lukavský Premysl"));
+
+    strHTML += "<img valign=center src=\"qrc:///flags/italian.png\">&nbsp;&nbsp;";
+    strHTML += tr("Italian version:") + " ";
+    strHTML += QString("<a href=\"http://www.astrofilipc.it\">%1</a><br/>").arg(tr("Fabio Papa"));
+
+    strHTML += "<img valign=center src=\"qrc:///flags/catalan.png\">&nbsp;&nbsp;";
+    strHTML += tr("Catalan version:") + " ";
+    strHTML += QString("<a href=\"mailto:astroemporda@gmail.com\">%1</a><br/>").arg(tr("Francesc Pruneda"));
+
+    strHTML += "<img valign=center src=\"qrc:///flags/german.png\">&nbsp;&nbsp;";
+    strHTML += tr("German version:") + " ";
+    strHTML += QString("<a href=\"mailto:kasakow@arcor.de\">%1</a><br/>").arg(tr("Wilhelm-Michael Kasakow"));
+
+    strHTML += "<img valign=center src=\"qrc:///flags/dutch.png\">&nbsp;&nbsp;";
+    strHTML += tr("Dutch version:") + " ";
+    strHTML += QString("<a href=\"http://www.geocities.com/me_gielis/\">%1</a> %2 ").arg(tr("Eelko Gielis"), tr("and"));
+    strHTML += QString("%1<br/>").arg(tr("Inge van de Stadt"));
+
+    strHTML += "<img valign=center src=\"qrc:///flags/taiwan.png\">&nbsp;&nbsp;";
+    strHTML += tr("Traditional Chinese version:") + " ";
+    strHTML += QString("<a href=\"http://www.aoc.nrao.edu/~whwang/\">%1</a><br/>").arg(tr("Wei-Hao Wang"));
+
+    strHTML += "<img valign=center src=\"qrc:///flags/portugal.png\">&nbsp;<img valign=center src=\"qrc:///flags/brazil.png\">&nbsp;&nbsp;";
+    strHTML += tr("Portuguese version:") + " ";
+    strHTML += QString("<a href=\"http://www.astrofotografia.biz/\">%1</a><br/>").arg(tr("Manuel Fernandes"));
+
+    strHTML += "<img valign=center src=\"qrc:///flags/romanian.png\">&nbsp;&nbsp;";
+    strHTML += tr("Romanian version:") + " ";
+    strHTML += QString("<a href=\"http://www.astronomy.ro/\">%1</a><br/>").arg(tr("Emil Kolbert and Valentin Stoica"));
+
+    strHTML += "<img valign=center src=\"qrc:///flags/russian.png\">&nbsp;&nbsp;";
+    strHTML += tr("Russian version:") + " ";
+    strHTML += QString("<a href=\"http://oleg.milantiev.com/\">%1</a><br/>").arg(tr("Oleg Milantiev"));
+
+    strHTML += "<img valign=center src=\"qrc:///flags/turkish.png\">&nbsp;&nbsp;";
+    strHTML += tr("Turkish version:") + " ";
+    strHTML += QString("<a href=\"http://www.physics.metu.edu.tr/\">%1</a><br/>").arg(tr("Rasid Tugral"));
+
+    ui->credits->setText(strHTML);
 }
 
 About::~About()
 {
     delete ui;
+}
+
+void About::setInternetCheck(bool check)
+{
+    if (m_InternetCheck != check)
+    {
+        m_InternetCheck = check;
+        ui->cbCheckUpdate->setChecked(check);
+        emit internetCheckChanged();
+    }
 }
 
 void About::setLanguage(QString lang)
@@ -93,8 +150,14 @@ void About::selectLanguage(int idx)
     setLanguage(ui->comboBox->itemData(idx).toString());
 }
 
+void About::setCheck(bool check)
+{
+    setInternetCheck(check);
+}
+
 void About::storeSettings()
 {
     QSettings settings;
     settings.setValue("Language", m_Language);
+    settings.setValue("InternetCheck", m_InternetCheck);
 }
--- DeepSkyStacker_Qt/About.h
+++ DeepSkyStacker_Qt/About.h
@@ -11,6 +11,7 @@ class About : public QDialog
 {
     Q_OBJECT
     Q_PROPERTY(QString language READ language WRITE setLanguage NOTIFY languageChanged);
+    Q_PROPERTY(bool internetCheck READ internetCheck WRITE setInternetCheck NOTIFY internetCheckChanged);
 
 public:
     explicit About(QWidget *parent = 0);
@@ -23,16 +24,26 @@ public:
 
     void setLanguage(QString lang);
 
+    bool internetCheck()
+    {
+        return m_InternetCheck;
+    }
+
+    void setInternetCheck(bool);
+
 signals:
     void languageChanged();
+    void internetCheckChanged();
 
 private slots:
     void storeSettings();
     void selectLanguage(int);
+    void setCheck(bool);
 
 private:
     Ui::About *ui;
     QString m_Language;
+    bool m_InternetCheck;
 };
 
 #endif // ABOUT_H
